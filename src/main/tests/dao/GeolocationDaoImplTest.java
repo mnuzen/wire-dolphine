b6bd03f16655fb7ea71178ac68daa276d5d804f9
Jevin G <thorncorona@gmail.com>
2020-07-30 12:57:29 -0700

    Add tests, fix init

--- src/main/tests/dao/GeolocationDaoImplTest.java
+++ src/main/tests/dao/GeolocationDaoImplTest.java
@@ -1,18 +1,33 @@
 package dao;
 
+import static org.junit.Assert.assertEquals;
+
+import com.google.netpcapanalysis.dao.GeolocationDaoImpl;
+import com.google.netpcapanalysis.interfaces.dao.GeolocationDao;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import org.junit.Before;
 import org.junit.Test;
 
-public class GeolocationDaoTest {
+public class GeolocationDaoImplTest {
 
-  @Setu
+  public static GeolocationDao geo;
+
+  @Before
   public void setup() {
-
+    geo = new GeolocationDaoImpl();
   }
 
   @Test
   public void invalidAddressReturnsUnknown() throws UnknownHostException {
     InetAddress test = InetAddress.getByName("127.0.0.1");
+    assertEquals(geo.getCountry(test), "unknown");
   }
+
+  @Test
+  public void ipReturnsUS() throws UnknownHostException {
+    InetAddress test = InetAddress.getByName("54.70.247.191");
+    assertEquals(geo.getCountry(test), "United States");
+  }
+
 }
