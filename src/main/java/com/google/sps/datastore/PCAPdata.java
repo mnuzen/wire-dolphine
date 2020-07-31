176f4bce0ff306daffcefbb98e23848da8aa34e2
Erik-Handeland <erikhandeland@gmail.com>
2020-07-30 17:56:41 -0800

    Combind get/set Datastore
    Combind seperate datastore classes and reformated code to match google style guide.

--- src/main/java/com/google/sps/datastore/PCAPdata.java
+++ src/main/java/com/google/sps/datastore/PCAPdata.java
@@ -11,30 +11,28 @@ public final class PCAPdata {
   int size;
   boolean flagged;
   int frequency;
-    
 
-  public PCAPdata(String source, String destination, String domain, 
-  String location, String protocol,int size, boolean flagged, int frequency) 
-  {
-  this.source = source;
-  this.destination = destination;
-  this.domain = domain;
-  this.location = location;
-  this.protocol = protocol;
-  this.size = size;
-  this.flagged = flagged;
-  this.frequency = frequency;
+  public PCAPdata(String source, String destination, String domain, String location, String protocol, 
+      int size, boolean flagged, int frequency) {
+    this.source = source;
+    this.destination = destination;
+    this.domain = domain;
+    this.location = location;
+    this.protocol = protocol;
+    this.size = size;
+    this.flagged = flagged;
+    this.frequency = frequency;
   }
 
-   public PCAPdata() {
-  this.source = null;
-  this.destination = null;
-  this.domain = null;
-  this.location = null;
-  this.protocol = null;
-  this.size = 0;
-  this.flagged = false;
-  this.frequency = 0;
+  public PCAPdata() {
+    this.source = null;
+    this.destination = null;
+    this.domain = null;
+    this.location = null;
+    this.protocol = null;
+    this.size = 0;
+    this.flagged = false;
+    this.frequency = 0;
   }
 
 }
