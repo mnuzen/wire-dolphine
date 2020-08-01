5699313bc32bc1bd0c869b4dbf1a5b763d2122f8
Melba Nuzen <mnuzen@caltech.edu>
2020-08-01 21:43:28 +0000

    Adding protocol detection

--- src/main/java/com/google/sps/servlets/PacketParserServlet.java
+++ src/main/java/com/google/sps/servlets/PacketParserServlet.java
@@ -45,10 +45,15 @@ import java.io.IOException;
 import java.io.*;
 
 
-/** Servlet that processes comments.*/
+/** Servlet that processes raw PCAP files within the repo. 
+   * We define MYIP to be the IP address of the local machine that was used to create the PCAP file, 
+     and OUTIP to be all of the IP addresses that machine connected with.
+   * allPCAP HashMap indexes all unique OUTIPs in a PCAP file to a PCAPdata object that stores needed attributes, 
+     including the number of connections between MYIP and OUTIP 
+   * We load datastore with all of the unique PCAPdata objects from our HashMap with a given entity tag.
+*/
 @WebServlet("/PCAP-data")
 public class PacketParserServlet extends HttpServlet {
-  ArrayList<PCAPdata> allData = new ArrayList<PCAPdata>();
   HashMap<String, PCAPdata> allPCAP = new HashMap<String, PCAPdata>();
   DatastoreService datastore = DatastoreServiceFactory.getDatastoreService(); //creates database
 
@@ -56,26 +61,31 @@ public class PacketParserServlet extends HttpServlet {
   //static final String FILENAME = "WEB-INF/chargen-udp.pcap";
   //static final String FILENAME = "WEB-INF/files/chargen-tcp.pcap";
   static final String FILENAME = "WEB-INF/files/smallFlows.pcap";
-  static final String MYIP = "192.168.3.131";
+
+  static final String MYIP = "192.168.3.131"; // for smallFlows.pcap 
+  static final String ENTITY_TAG = "file_1";
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    parseRaw();
-    addToPCAPDao();
+    parseRaw(FILENAME);
+    putDatastore(ENTITY_TAG);
   }
 
-  /* Reads PCAP file (from filename) as a stream and puts into datastore. */
-  public void parseRaw() throws IOException {
-    final InputStream stream = new FileInputStream(FILENAME);
+  /* Reads PCAP file (from file name) as a stream and puts unique connections into allPCAP HashMap. */
+  public void parseRaw(String file) throws IOException {
+    final InputStream stream = new FileInputStream(file);
     final Pcap pcap = Pcap.openStream(stream);
 
     pcap.loop(new PacketHandler() {
       @Override
       public boolean nextPacket(final Packet packet) throws IOException {
-      if(packet.hasProtocol(Protocol.IPv4)) {
+      if(packet.hasProtocol(Protocol.IPv4)) {// is the first packet always (MYIP, OUTIP)?
         IPPacket ip = (IPPacket) packet.getPacket(Protocol.IPv4);
-        String protocol = "IPv4"; // is the first packet always (MYIP, OUTIP)?
+        String protocol = getProtocol(ip); 
         String OUTIP = "";
+
+        Buffer buffer = ip.getPayload();
+        int size = buffer.getReadableBytes();
             
         //The IP addresses involved
         String srcip = ip.getSourceIP();
@@ -88,20 +98,13 @@ public class PacketParserServlet extends HttpServlet {
             OUTIP = srcip;
         }
 
-        if (packet.hasProtocol(Protocol.UDP)) {
-          protocol = "UDP";
-        }
-        else if (packet.hasProtocol(Protocol.TCP)) {
-          protocol = "TCP";
-        }
-
         // PCAPdata tempPCAP = new PCAPdata(source, destination, domain, location, protocol, size, flagged, frequency);
         if (allPCAP.containsKey(OUTIP)){
-            PCAPdata tempPCAP = new PCAPdata(MYIP, OUTIP, "", "", protocol, 2, false, allPCAP.get(OUTIP).getFrequency()+1); // how to use increment frequency method instead of getting freq++?
+            PCAPdata tempPCAP = new PCAPdata(MYIP, OUTIP, "", "", protocol, size, false, allPCAP.get(OUTIP).getFrequency()+1); // how to use increment frequency method instead of getting freq++?
             allPCAP.put(OUTIP, tempPCAP);
         }
         else {
-            PCAPdata tempPCAP = new PCAPdata(MYIP, OUTIP, "", "", protocol, 2, false, 1);
+            PCAPdata tempPCAP = new PCAPdata(MYIP, OUTIP, "", "", protocol, size, false, 1);
             allPCAP.put(OUTIP, tempPCAP);
         }
       }
@@ -109,25 +112,65 @@ public class PacketParserServlet extends HttpServlet {
     }
    });
    pcap.close();
-  } // end of parseRaw function
+  } 
 
-  public void addToPCAPDao(){
+  /* Determine the protocol of IPv4 packet; if unknown, then returns "IPv4" as protocol.
+    * Checks for the following protocols — ICMP, IGMP, TCP, UDP, SCTP, SIP, SDP, ETHERNET_II, SLL, IPv4, RTP —
+    * which are the only protocols supported by pkts library. */
+  public String getProtocol(IPPacket packet) throws IOException {
+    String protocol = "IPv4";
+    if (packet.hasProtocol(Protocol.UDP)) {
+      protocol = "UDP";
+    }
+    else if (packet.hasProtocol(Protocol.TCP)) {
+      protocol = "TCP";
+    } 
+    else if (packet.hasProtocol(Protocol.ICMP)) {
+      protocol = "ICMP";
+    }
+    else if (packet.hasProtocol(Protocol.IGMP)) {
+      protocol = "IGMP";
+    }
+    else if (packet.hasProtocol(Protocol.SCTP)) {
+      protocol = "SCTP";
+    }
+    else if (packet.hasProtocol(Protocol.SIP)) {
+      protocol = "SIP";
+    }
+    else if (packet.hasProtocol(Protocol.SDP)) {
+      protocol = "SDP";
+    }
+    else if (packet.hasProtocol(Protocol.ETHERNET_II)) {
+      protocol = "ETHERNET_II";
+    }
+    else if (packet.hasProtocol(Protocol.SLL)) {
+      protocol = "SLL";
+    }
+    return protocol;
+  }
+
+  /* Adds all processed packets to datastore through GenericPCAPDao*/
+  public void putDatastore(String tag){
     for (PCAPdata temp : allPCAP.values()) {
       GenericPCAPDao data = new GenericPCAPDao();
-      data.setPCAPObjects(temp, "file_1");
+      data.setPCAPObjects(temp, tag);
     }
-  } // end of addToPCAPDao function
+  } 
 
 
 } // end of PacketParserServlet class
 
+
+
+
+/* If packet has UDP or TCP protocol, fetch the port numbers (source/destination).*/
 /**    if (packet.hasProtocol(Protocol.UDP)) {
             UDPPacket udpPacket = (UDPPacket) packet.getPacket(Protocol.UDP);
             int dstport = udpPacket.getDestinationPort();
             int srcport = udpPacket.getSourcePort();
           }
           else if (packet.hasProtocol(Protocol.TCP)) {
-            TCPPacket tcpPacket = (TCPPacket) packet.getPacket(Protocol.TCP);
+            TCPPacket tcpPacket = (Tmvn cCPPacket) packet.getPacket(Protocol.TCP);
             int dstport = tcpPacket.getDestinationPort();
             int srcport = tcpPacket.getSourcePort();
           }*/
