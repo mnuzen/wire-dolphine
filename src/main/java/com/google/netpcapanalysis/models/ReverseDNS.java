97d4b6b01274fe7622fb6288ab8dd998de840dc1
Jevin G <thorncorona@gmail.com>
2020-07-31 17:25:18 -0700

    Revised according to review

--- src/main/java/com/google/netpcapanalysis/models/ReverseDNS.java
+++ src/main/java/com/google/netpcapanalysis/models/ReverseDNS.java
@@ -6,11 +6,36 @@ public class ReverseDNS {
   public String record;
 
   public ReverseDNS() {
-    authority = false;
-    server = false;
+    this("", false, false);
   }
 
   public ReverseDNS(String record, boolean server, boolean authority) {
+    this.record = record;
+    this.authority = authority;
+    this.server = server;
+  }
 
+  public boolean isAuthority() {
+    return authority;
+  }
+
+  public void setAuthority(boolean authority) {
+    this.authority = authority;
+  }
+
+  public boolean isServer() {
+    return server;
+  }
+
+  public void setServer(boolean server) {
+    this.server = server;
+  }
+
+  public String getRecord() {
+    return record;
+  }
+
+  public void setRecord(String record) {
+    this.record = record;
   }
 }
